8a7b5030cef679cfacbca92a95d503d7ff1e45c1
Gabriel Santos <gabriellima14k@outlook.com>
2023-09-08 10:04:18 -0300

    Knight

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -12,6 +12,11 @@ public class Bishop extends ChessPiece {
 	}
 
 	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 
@@ -58,10 +63,5 @@ public class Bishop extends ChessPiece {
 		}
 
 		return mat;
-	}
-
-	@Override
-	public String toString() {
-		return "B";
 	}
 }
